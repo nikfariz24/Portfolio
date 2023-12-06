ae5da1555dc74c92e8fe38f2a60de7b70715119b
Nik Fariz Nik Ahmad Aziz <nikfariz24@gmail.com>
2023-12-06 14:13:12 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Divert to linkedIn profile upon clicking linkedIn icon
+document.getElementById("linkedin-link").addEventListener('click',function(linkedIn){
+    window.open("https://www.linkedin.com/in/nik-fariz-nik-ahmad-aziz-a89168141/");
+})
+
+// Divert to GitHub profile upon clicking linkedIn icon
+document.getElementById("github").addEventListener('click',function(github){
+    window.open("https://github.com/nikfariz24");
+})
+
